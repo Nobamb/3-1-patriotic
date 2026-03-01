b91045975f8fc68c7b205573d1dda1afff5cfb80
nobamb <jsh12345644@gmail.com>
2026-03-01 22:01:12 +0900

    quiz 가져올 때 default 형식으로 가져오고, initQuiz => quiz로 변경

--- javascript/main.js
+++ javascript/main.js
@@ -9,7 +9,7 @@ import setupAudio from "./func/setupAudio.js";
 import setupMenu from "./func/setupMenu.js";
 import setupSearch from "./func/setupSearch.js";
 
-import { initQuiz } from "./func/quiz.js";
+import quiz from "./func/quiz.js";
 
 // 1. UI 및 보조 기능 초기화
 setupMenu();   // 모바일 햄버거 메뉴 세팅
@@ -17,4 +17,4 @@ setupSearch(); // 검색 기능(구글, 나무위키) 세팅
 setupAudio();  // 애국가 BGM 세팅
 
 // 2. 메인 퀴즈 시스템 초기화 및 시작
-initQuiz();
+quiz();
