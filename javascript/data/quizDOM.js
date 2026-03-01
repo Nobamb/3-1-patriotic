7c4c230fec1d0299800fa1ced0c5a0de311dcb9e
nobamb <jsh12345644@gmail.com>
2026-03-01 21:50:21 +0900

    quiz 관련 DOM 데이터 따로 모듈화

---
+++ javascript/data/quizDOM.js
@@ -0,0 +1,14 @@
+const quizDOM = {
+  // --- 2. DOM 요소 선택 ---
+  quizContainer: document.getElementById("quizContainer"),
+  navContainer: document.getElementById("navContainer"),
+  resultContainer: document.getElementById("resultContainer"),
+  btnPrev: document.getElementById("btnPrev"),
+  btnNext: document.getElementById("btnNext"),
+  resultTitle: document.getElementById("resultTitle"),
+  resultDesc: document.getElementById("resultDesc"),
+};
+
+
+// export
+export default quizDOM
